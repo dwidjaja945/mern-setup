28d95e4fbb222821fb967526764d2e77fc4183fb
Dylan Widjaja <dwidjaja945@gmail.com>
2018-05-10 15:26:12 -0700

    added script to start both back and front end servers

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const cors = require("cors");
 const PORT = process.env.PORT || 9000;
 
 const app = express();
 
+//This allows any computer/server to access my server. 
+//NOT to use in production
+app.use(cors());
+
+app.get('/user-data', (req, res) => {
+    const user = {
+        name: 'Dylan Widjaja',
+        email: 'dylan@dylan.com'
+    };
+    res.send(user);
+});
+
 app.get('/', function(req, res) {
     res.send('<h1>App is working</h1>');
 });
-
-
-
 
 app.listen(PORT, () => {
     console.log('Server listening on port: ' , PORT);
